ec93a1c28b853f17e59646595e21e57124098f7c
Alexey Kulish <alexkulish1993@gmail.com>
2022-07-28 13:27:36 +0300

    Setup LaunchScreen

--- NewsApp/Models/News.swift
+++ NewsApp/Models/News.swift
@@ -9,17 +9,13 @@ import Foundation
 
 struct NewsModel: Decodable {
     let news: [News]
-//    let totalCount: Int
 }
 
 struct News: Decodable {
-//    let id: Int
     let title: String
     let description: String
     let publishedDate: String
-//    let url: String
     let fullUrl: String
     let titleImageUrl: String
     let categoryType: String
-
 }
